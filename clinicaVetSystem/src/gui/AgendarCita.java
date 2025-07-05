713513ea83536394aa5c8a2198fc5d11a89948e3
sebas <sebas@DESKTOP-K0QVLHJ>
2025-07-05 13:58:51 -0500

    Commit Agendar cita

--- clinicaVetSystem/src/gui/AgendarCita.java
+++ clinicaVetSystem/src/gui/AgendarCita.java
@@ -15,26 +15,20 @@ import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import java.awt.SystemColor;
 
 public class AgendarCita extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
 	private JButton cancelButton;
-	private JLabel lblNewLabel;
-	private JComboBox cboVeterinarios;
-	private JLabel lblDnicliente;
-	private JTextField txtDniCliente;
-	private JScrollPane scrollPane;
-	private JTable tblAgendar;
-	private JButton btnAgendar;
-	private JScrollPane scrollPane_1;
-	private JTable tblServicio;
-	private JLabel lblNewLabel_1;
-	private JTextField txtServicio;
-	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel_3;
+	private JTextField txtDniCli;
 	private JTextField txtHora;
+	private JTable tblCitas;
+	private JTable tblServi;
 
 	/**
 	 * Launch the application.
@@ -54,84 +48,90 @@ public class AgendarCita extends JDialog implements ActionListener {
 	 */
 	public AgendarCita() {
 		setTitle("Agendar Cita");
-		setBounds(100, 100, 924, 672);
+		setBounds(100, 100, 643, 570);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBackground(new Color(255, 250, 250));
+		contentPanel.setBackground(new Color(255, 255, 255));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		{
-			lblNewLabel = new JLabel("Veterinarios:");
-			lblNewLabel.setBounds(431, 87, 76, 14);
-			contentPanel.add(lblNewLabel);
-		}
-		{
-			cboVeterinarios = new JComboBox();
-			cboVeterinarios.setBounds(517, 83, 109, 22);
-			contentPanel.add(cboVeterinarios);
-		}
-		{
-			lblDnicliente = new JLabel("DNI-Cliente:");
-			lblDnicliente.setBounds(431, 30, 76, 14);
-			contentPanel.add(lblDnicliente);
-		}
-		{
-			txtDniCliente = new JTextField();
-			txtDniCliente.setBounds(517, 27, 109, 20);
-			contentPanel.add(txtDniCliente);
-			txtDniCliente.setColumns(10);
-		}
-		{
-			scrollPane = new JScrollPane();
-			scrollPane.setBounds(26, 248, 600, 316);
-			contentPanel.add(scrollPane);
-			{
-				tblAgendar = new JTable();
-				scrollPane.setViewportView(tblAgendar);
-			}
-		}
-		{
-			btnAgendar = new JButton("Agendar");
-			btnAgendar.setBackground(new Color(255, 192, 203));
-			btnAgendar.setBounds(709, 55, 114, 48);
-			contentPanel.add(btnAgendar);
-		}
-		{
-			scrollPane_1 = new JScrollPane();
-			scrollPane_1.setBounds(10, 11, 390, 151);
-			contentPanel.add(scrollPane_1);
-			{
-				tblServicio = new JTable();
-				scrollPane_1.setViewportView(tblServicio);
-			}
-		}
-		{
-			lblNewLabel_1 = new JLabel("Servicio:");
-			lblNewLabel_1.setBounds(45, 185, 56, 22);
-			contentPanel.add(lblNewLabel_1);
-		}
-		{
-			txtServicio = new JTextField();
-			txtServicio.setBounds(111, 185, 174, 22);
-			contentPanel.add(txtServicio);
-			txtServicio.setColumns(10);
-		}
-		{
-			lblNewLabel_2 = new JLabel("Fecha:");
-			lblNewLabel_2.setBounds(345, 189, 46, 14);
-			contentPanel.add(lblNewLabel_2);
-		}
-		{
-			lblNewLabel_3 = new JLabel("Hora:");
-			lblNewLabel_3.setBounds(473, 189, 46, 14);
-			contentPanel.add(lblNewLabel_3);
-		}
-		{
-			txtHora = new JTextField();
-			txtHora.setBounds(517, 178, 109, 28);
-			contentPanel.add(txtHora);
-			txtHora.setColumns(10);
-		}
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(192, 192, 192));
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "DATOS", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBounds(13, 15, 292, 240);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Dueño: ");
+		lblNewLabel.setBounds(35, 30, 46, 14);
+		panel.add(lblNewLabel);
+		
+		txtDniCli = new JTextField();
+		txtDniCli.setBounds(79, 27, 175, 20);
+		panel.add(txtDniCli);
+		txtDniCli.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Fecha");
+		lblNewLabel_1.setBounds(35, 62, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JComboBox cboDia = new JComboBox();
+		cboDia.setBounds(79, 58, 41, 22);
+		panel.add(cboDia);
+		cboDia.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		
+		JComboBox cboMes = new JComboBox();
+		cboMes.setBounds(130, 58, 46, 22);
+		panel.add(cboMes);
+		cboMes.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		
+		JComboBox cboAño = new JComboBox();
+		cboAño.setBounds(187, 58, 66, 22);
+		panel.add(cboAño);
+		cboAño.setModel(new DefaultComboBoxModel(new String[] {"2025", "2026", "2027", "2028", "2029", "2030"}));
+		
+		JLabel lblNewLabel_2 = new JLabel("Hora: ");
+		lblNewLabel_2.setBounds(35, 87, 46, 14);
+		panel.add(lblNewLabel_2);
+		
+		txtHora = new JTextField();
+		txtHora.setBounds(79, 87, 173, 20);
+		panel.add(txtHora);
+		txtHora.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Veterinario");
+		lblNewLabel_3.setBounds(29, 120, 68, 14);
+		panel.add(lblNewLabel_3);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(99, 116, 150, 22);
+		panel.add(comboBox);
+		
+		JLabel lblNewLabel_4 = new JLabel("Servicios");
+		lblNewLabel_4.setBounds(29, 155, 55, 14);
+		panel.add(lblNewLabel_4);
+		
+		JComboBox cboServi = new JComboBox();
+		cboServi.setBounds(99, 151, 150, 22);
+		panel.add(cboServi);
+		
+		JButton btnRegiCita = new JButton("Registrar");
+		btnRegiCita.setBounds(74, 197, 116, 23);
+		panel.add(btnRegiCita);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(14, 261, 601, 225);
+		contentPanel.add(scrollPane);
+		
+		tblCitas = new JTable();
+		scrollPane.setViewportView(tblCitas);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(311, 20, 301, 231);
+		contentPanel.add(scrollPane_1);
+		
+		tblServi = new JTable();
+		scrollPane_1.setViewportView(tblServi);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
