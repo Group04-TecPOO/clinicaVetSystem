4faf6bf70d94a3b5a27e1fa0b8031e1c2443a479
Cecilia <Cecilia@DESKTOP-H4B6V17>
2025-07-08 12:40:05 -0500

    comit jean Arraays

---
+++ clinicaVetSystem/src/ArrayClases/ArrayCitas.java
@@ -0,0 +1,91 @@
+package ArrayClases;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import clases.Citas;
+import clases.Cliente;
+import connection.ConexionMysql;
+
+public class ArrayCitas {
+
+	public ArrayList<Citas> listarCitas(){
+		ArrayList<Citas> lista = new ArrayList<Citas>();
+		try {
+			CallableStatement csta = ConexionMysql.getConexion().prepareCall("{call citas_Listar()}");
+			ResultSet rs = csta.executeQuery();
+			Citas citas;
+			while (rs.next()) {
+				citas = new Citas(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getInt(7),rs.getString(8),rs.getString(9));
+				lista.add(citas);
+			}
+		} catch (Exception e) {}
+		return lista;
+	}
+	
+	public ArrayList<Citas> consultarIDCita(int IDCita){
+		ArrayList<Citas> lista = new ArrayList<Citas>();
+		try {
+			CallableStatement csta = ConexionMysql.getConexion().prepareCall("{call citas_ConsultarIDCita(?)}");
+			csta.setInt(1, IDCita);
+			ResultSet rs = csta.executeQuery();
+			Citas citas;
+			while (rs.next()) {
+				citas = new Citas(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getInt(7),rs.getString(8),rs.getString(9));
+				lista.add(citas);
+			}
+		} catch (Exception e) {}
+		return lista;
+	}
+	
+	public void Insertar(Citas citas) {
+		try {
+			Connection cnx = ConexionMysql.getConexion();
+			CallableStatement csta = cnx.prepareCall("{call citas_Insertar(?,?,?,?,?,?,?,?,?)}");
+			csta.setInt(1, citas.getIdCita());
+			csta.setString(2, citas.getDia());
+			csta.setString(3, citas.getMes());
+			csta.setString(4, citas.getAño());
+			csta.setString(5, citas.getHora());
+			csta.setString(6, citas.getDniCliente());
+			csta.setInt(7, citas.getIdMascota());
+			csta.setString(8, citas.getDniEmpleado());
+			csta.setString(9, citas.getIdServicio());
+			csta.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("ERROR" + e);
+		}
+	}
+	
+	public void Modificar(Citas citas) {
+		try {
+			Connection cnx = ConexionMysql.getConexion();
+			CallableStatement csta = cnx.prepareCall("{call citas_Modificar(?,?,?,?,?,?,?;?,?}");
+			csta.setInt(1, citas.getIdCita());
+			csta.setString(2, citas.getDia());
+			csta.setString(3, citas.getMes());
+			csta.setString(4, citas.getAño());
+			csta.setString(5, citas.getHora());
+			csta.setString(6, citas.getDniCliente());
+			csta.setInt(7, citas.getIdMascota());
+			csta.setString(8, citas.getDniEmpleado());
+			csta.setString(9, citas.getIdServicio());
+			csta.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("ERROR" + e);
+		}
+	}
+	public void Eliminar(int IdCita) {
+		try {
+			Connection cnx = ConexionMysql.getConexion();
+			CallableStatement csta = cnx.prepareCall("{call citas_Eliminar(?)}");
+			csta.setInt(1, IdCita);
+			csta.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("ERROR" + e);
+		}
+	}
+	
+}
---
+++ clinicaVetSystem/src/clases/Citas.java
@@ -0,0 +1,82 @@
+package clases;
+
+public class Citas {
+
+	private int IdCita;
+	private String Dia;
+	private String Mes;
+	private String Año;
+	private String Hora;
+	private String DniCliente;
+	private int IdMascota;
+	private String DniEmpleado;
+	private String IdServicio;
+	public Citas(int idCita, String dia, String mes, String año, String hora, String dniCliente, int idMascota,
+			String dniEmpleado, String idServicio) {
+		super();
+		IdCita = idCita;
+		Dia = dia;
+		Mes = mes;
+		Año = año;
+		Hora = hora;
+		DniCliente = dniCliente;
+		IdMascota = idMascota;
+		DniEmpleado = dniEmpleado;
+		IdServicio = idServicio;
+	}
+	public int getIdCita() {
+		return IdCita;
+	}
+	public void setIdCita(int idCita) {
+		IdCita = idCita;
+	}
+	public String getDia() {
+		return Dia;
+	}
+	public void setDia(String dia) {
+		Dia = dia;
+	}
+	public String getMes() {
+		return Mes;
+	}
+	public void setMes(String mes) {
+		Mes = mes;
+	}
+	public String getAño() {
+		return Año;
+	}
+	public void setAño(String año) {
+		Año = año;
+	}
+	public String getHora() {
+		return Hora;
+	}
+	public void setHora(String hora) {
+		Hora = hora;
+	}
+	public String getDniCliente() {
+		return DniCliente;
+	}
+	public void setDniCliente(String dniCliente) {
+		DniCliente = dniCliente;
+	}
+	public int getIdMascota() {
+		return IdMascota;
+	}
+	public void setIdMascota(int idMascota) {
+		IdMascota = idMascota;
+	}
+	public String getDniEmpleado() {
+		return DniEmpleado;
+	}
+	public void setDniEmpleado(String dniEmpleado) {
+		DniEmpleado = dniEmpleado;
+	}
+	public String getIdServicio() {
+		return IdServicio;
+	}
+	public void setIdServicio(String idServicio) {
+		IdServicio = idServicio;
+	}
+	
+}
